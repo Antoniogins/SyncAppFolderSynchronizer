b5a9ac3d7be8ff3ef7de2d441310679b5ee6743b
maripazmg8 <pazmeii37@gmail.com>
2022-12-03 18:34:48 +0100

    Update ClienteGUI.java

--- src/main/java/com/syncapp/cliente/ClienteGUI.java
+++ src/main/java/com/syncapp/cliente/ClienteGUI.java
@@ -1,8 +1,10 @@
 package com.syncapp.cliente;
 
-
 import java.awt.*;
 import javax.swing.*;
+
+import com.syncapp.server.SyncAppServer;
+import com.syncapp.utility.Util;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -21,55 +23,58 @@ public class ClienteGUI extends JFrame implements ActionListener {
 
 	// Al constructor de la clase vista le pasamos el cliente
 
-
-
-
-
 	public ClienteGUI(String[] args) {
 		super("Cliente SyncApp");
 
-		//Iniciamos en primer lugar el servidor
+		// Iniciamos en primer lugar el servidor
 		cliente = new SyncAppCliente(args);
 
-
-
-		Container contenedor = super.getContentPane();
+		Container contenedor = getContentPane();
 		contenedor.setLayout(new BorderLayout());
 
-
-		JPanel pcentral = new JPanel(new BorderLayout());//este contiene todos los paneles que vamos a crear
+		JPanel pcentral = new JPanel(new BorderLayout());
 		JPanel pbotones = new JPanel(new FlowLayout());
 		JPanel pinferior = new JPanel(new FlowLayout());
 
-		//panel de información central
+		// panel de información central
 		JPanel pinformacion = new JPanel(new BorderLayout());
+		// aquí se añade lo que sale por consola al panel de informacion
+
 		pcentral.add(pinformacion);
 
+		/*
+		 * //paneles de la parte inferior
+		 * JPanel salidatexto = new JPanel (new FlowLayout());
+		 * salidatexto.add(new JLabel (String.valueOf(cliente.getTimeOffset())));
+		 * 
+		 * 
+		 * JPanel conexion = new JPanel (new FlowLayout());
+		 * conexion.add(new JLabel(String.valueOf(cliente.getRemoteServer())));
+		 * 
+		 * 
+		 * pinferior.add(salidatexto);
+		 * pinferior.add(conexion);
+		 * 
+		 */
 
-
-
-
-
-		//creamos los botones y los añadimos al panel
-		bservidor = new JButton ("Seleccionar servidor");
+		// creamos los botones y los añadimos al panel
+		bservidor = new JButton("Seleccionar servidor");
 		bservidor.addActionListener(this);
 
-		busuario = new JButton ("Seleccionar usuario");
+		busuario = new JButton("Seleccionar usuario");
 		busuario.addActionListener(this);
 
-		bcarpetalocal = new JButton ("Seleccionar carpeta local");
-		bcarpetalocal.addActionListener (this);
+		bcarpetalocal = new JButton("Seleccionar carpeta local");
+		bcarpetalocal.addActionListener(this);
 
-		bcarpetaremota = new JButton ("seleccionar carpeta remota");
+		bcarpetaremota = new JButton("seleccionar carpeta remota");
 		bcarpetaremota.addActionListener(this);
 
-		bejecutar = new JButton ("Ejecutar");
+		bejecutar = new JButton("Ejecutar");
 		bejecutar.addActionListener(this);
 
-		brestart = new JButton ("Restart");
+		brestart = new JButton("Restart");
 		brestart.addActionListener(this);
-
-
 
 		pbotones.add(bservidor);
 		pbotones.add(busuario);
@@ -78,19 +83,15 @@ public class ClienteGUI extends JFrame implements ActionListener {
 		pbotones.add(bejecutar);
 		pbotones.add(brestart);
 
-
-
-
-
-		//Se lo añadimos al borderlayout
+		// Se lo añadimos al borderlayout
 		contenedor.add(pcentral, BorderLayout.CENTER);
 		contenedor.add(pbotones, BorderLayout.NORTH);
 		contenedor.add(pinferior, BorderLayout.SOUTH);
 
-		//Esto siempre hay que ponerlo
+		// Esto siempre hay que ponerlo
 
-		setTitle ("SyncAppGUI");
-		setSize (800,800);
+		setTitle("Ponte titulo");
+		setSize(800, 800);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -100,9 +101,7 @@ public class ClienteGUI extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		JButton origen = (JButton) e.getSource();
 
-
-
-		if(origen == bservidor) {
+		if (origen == bservidor) {
 
 			System.out.println("funciona boton servidor");
 
@@ -123,87 +122,61 @@ public class ClienteGUI extends JFrame implements ActionListener {
 				System.out.println("y value: " + puerto.getText());
 			}
 
+			// bservidor.add(pop_servidor);
+			// pop_servidor.add(String.valueOf(ip));
 
+			// JLabel ip = new JLabel ("Introduce la ip del servidor");
+			// ip.add(new JTextField(15));
+			// pop_servidor.add(String.valueOf(puerto));
+			// JLabel puerto = new JLabel ("Introduce el puerto del servidor");
+			// ip.add(new JTextField(15));
+			// obtener IP y pasarla como parametro
+			// sac.restart
 
-//			bservidor.add(pop_servidor);
-//			pop_servidor.add(String.valueOf(ip));
-
-
-//			JLabel ip = new JLabel ("Introduce la ip del servidor");
-//			ip.add(new JTextField(15));
-//			pop_servidor.add(String.valueOf(puerto));
-//			JLabel puerto = new JLabel ("Introduce el puerto del servidor");
-//			ip.add(new JTextField(15));
-			//obtener IP y pasarla como parametro
-			//sac.restart
-
-		}
-		else if(origen == bcarpetalocal) {
+		} else if (origen == bcarpetalocal) {
 
 			System.out.println("funciona boto carpeta local");
 			bcarpetalocal.add(pop_carpeta_local);
 			pop_carpeta_local.add(String.valueOf(carpetalocal));
 			JLabel carpetalocal = new JLabel("Selecciona la carpeta local que quieres sincronizar");
 			carpetalocal.add(new JTextField(200));
-			//sac.reload
+			// sac.reload
 
-		}
-		else if(origen == bcarpetaremota) {
+		} else if (origen == bcarpetaremota) {
 
 			System.out.println("funciona boton carpeta remota");
 
-
-
 			pop_carpeta_remota.add(String.valueOf(carpetaremota));
-			JLabel carpetaremota = new JLabel ("Introduce la carpeta remota que quieres sincrinzar");
+			JLabel carpetaremota = new JLabel("Introduce la carpeta remota que quieres sincrinzar");
 			ip.add(new JTextField(200));
-			//sac.reload
+			// sac.reload
 
-
-		}
-		else if(origen == busuario) {
-
+		} else if (origen == busuario) {
 
 			System.out.println("funciona boton usuario");
 
-
 			busuario.add(pop_usuario);
 			pop_usuario.add(String.valueOf(usuario));
-			JLabel usuario = new JLabel ("Introduce tu usuario");
+			JLabel usuario = new JLabel("Introduce tu usuario");
 			usuario.add(new JTextField(15));
 			pop_usuario.add(String.valueOf(password));
-			JLabel password = new JLabel ("Introduce tu contraseña");
+			JLabel password = new JLabel("Introduce tu contraseña");
 			usuario.add(new JTextField(15));
-			//sac.restart
+			// sac.restart
 
-		}
-		else if(origen == brestart) {
+		} else if (origen == brestart) {
 
 			System.out.println("funciona boton restart");
 
+			// sac.restart;
 
-			//sac.restart;
-
-		}
-		else if(origen == bejecutar) {
+		} else if (origen == bejecutar) {
 
 			System.out.println("funciona boton ejecutar");
 
-
-
-
-			//sa.ejecutar;
+			// sa.ejecutar;
 		}
 
-
-
 	}
-
-
-
-
-
-
-
 
 }
